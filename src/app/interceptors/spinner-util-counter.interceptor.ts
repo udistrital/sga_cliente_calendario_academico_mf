f310d010d3fbf70c704e8faa04f457f51308fe6b
Alvaro Zarabanda <alvaroalejo25@gmail.com>
2026-08-14 15:31:51 -0500

    fix: coordina carga global y notificaciones
    Se contabilizan las solicitudes activas y se difieren mensajes y alertas hasta finalizar el estado global de carga.

--- src/app/interceptors/spinner-util-counter.interceptor.ts
+++ src/app/interceptors/spinner-util-counter.interceptor.ts
@@ -2,33 +2,62 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable, finalize } from 'rxjs';
 import { SpinnerUtilService } from 'spinner-util';
+import { GlobalLoadingStateService } from '../services/global-loading-state.service';
 
 @Injectable()
 export class SpinnerUtilCounterInterceptor implements HttpInterceptor {
   private activeRequests = 0;
+  private showTimeout?: ReturnType<typeof setTimeout>;
   private hideTimeout?: ReturnType<typeof setTimeout>;
+  private visible = false;
 
-  constructor(private spinnerService: SpinnerUtilService) {}
+  constructor(
+    private spinnerService: SpinnerUtilService,
+    private loadingState: GlobalLoadingStateService,
+  ) {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.activeRequests += 1;
+    this.loadingState.markBusy();
     if (this.hideTimeout) {
       clearTimeout(this.hideTimeout);
       this.hideTimeout = undefined;
     }
-    this.spinnerService.show();
+    this.scheduleShow();
 
     return next.handle(req).pipe(
       finalize(() => {
         this.activeRequests = Math.max(0, this.activeRequests - 1);
         if (this.activeRequests === 0) {
+          if (this.showTimeout) {
+            clearTimeout(this.showTimeout);
+            this.showTimeout = undefined;
+          }
           this.hideTimeout = setTimeout(() => {
+            this.hideTimeout = undefined;
             if (this.activeRequests === 0) {
-              this.spinnerService.hide();
+              if (this.visible) {
+                this.visible = false;
+                this.spinnerService.hide();
+              }
+              this.loadingState.markIdle();
             }
           }, 150);
         }
       })
     );
+  }
+
+  private scheduleShow(): void {
+    if (this.visible || this.showTimeout) {
+      return;
+    }
+    this.showTimeout = setTimeout(() => {
+      this.showTimeout = undefined;
+      if (this.activeRequests > 0 && !this.visible) {
+        this.visible = true;
+        this.spinnerService.show();
+      }
+    });
   }
 }
